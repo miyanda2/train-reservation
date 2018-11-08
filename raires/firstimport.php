8c7b5d5a80d04c239bc45d36f64d37b903705792
Sadiq Hassan <abubakarhassan59@gmail.com>
2018-11-08 04:28:06 +0100

    Moved files to the root directory

--- raires/firstimport.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host="localhost"; // Host name
-$username="railres"; // Mysql username
-$password="Railres@123"; // Mysql password
-$db_name="railres"; // Database name
-
-$conn=mysqli_connect("$host", "$username", "$password")or die("cannot connect");
-
-?>
