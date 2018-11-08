40121185f5f3a5d1164ef6a5851e60aee93e5705
Sadiq Hassan <abubakarhassan59@gmail.com>
2018-11-08 04:21:09 +0100

    Updated config file

--- raires/firstimport.php
+++ raires/firstimport.php
@@ -1,7 +1,7 @@
 <?php
 $host="localhost"; // Host name
-$username="root"; // Mysql username
-$password=""; // Mysql password
+$username="railres"; // Mysql username
+$password="Railres@123"; // Mysql password
 $db_name="railres"; // Database name
 
 $conn=mysqli_connect("$host", "$username", "$password")or die("cannot connect");
