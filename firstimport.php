9800ffff0cbc176dee95233143ce5e59ba575b35
Sadiq Hassan <abubakarhassan59@gmail.com>
2018-11-08 04:40:44 +0100

    Updated the copyright and designer name

---
+++ firstimport.php
@@ -0,0 +1,9 @@
+<?php
+$host="localhost"; // Host name
+$username="railres"; // Mysql username
+$password="Railres@123"; // Mysql password
+$db_name="railres"; // Database name
+
+$conn=mysqli_connect("$host", "$username", "$password")or die("cannot connect");
+
+?>
